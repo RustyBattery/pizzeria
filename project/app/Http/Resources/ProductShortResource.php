1a3a7bc3b87f10c568c5b387fb54583b9aeec98d
Rita Adukova <adukova.meow@gmail.com>
2024-08-13 03:09:26 +0300

    fix product resources

--- project/app/Http/Resources/ProductShortResource.php
+++ project/app/Http/Resources/ProductShortResource.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @method static make(Product $product)
+ * @method static collection(Collection|Product[] $products)
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $price
+ * @property Category $category
+ * @property Image[] $images
+ * @property bool $in_stock
+ */
 class ProductShortResource extends JsonResource
 {
     public function toArray(Request $request): array
@@ -13,6 +28,7 @@ class ProductShortResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
+            'price' => bcdiv($this->price / 100, 1, 2),
             'category' => CategoryResource::make($this->category),
             'images' => ImageResource::collection($this->images),
             'in_stock' => (bool)$this->in_stock,
