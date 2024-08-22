5db8a4b48d7141ee86eb0331295fa3837c69c870
Rita Adukova <adukova.meow@gmail.com>
2024-08-22 23:50:10 +0300

    add policies (refactor forbidden exception)

--- project/app/Exceptions/Handler.php
+++ project/app/Exceptions/Handler.php
@@ -13,6 +13,7 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Request;
 use ReflectionClass;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -123,7 +124,7 @@ class Handler extends ExceptionHandler
             return parent::render($request, $e);
         });
 
-        $this->renderable(function (ForbiddenException $e, Request $request) {
+        $this->renderable(function (AccessDeniedHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json(['message' => 'Forbidden'], ResponseAlias::HTTP_FORBIDDEN);
             }
